5661196a529c77f93ea75af0ba0b3816f161f7b4
adfar-136 <adfarrasheed136@gmail.com>
2025-05-11 11:46:18 +0530

    done on Adfar

--- index.js
+++ index.js
@@ -15,13 +15,9 @@ let todos = [];
 // Routes
 // Get all todos
 app.get('/todos', (req, res) => {
-
-  res.json(todos,"Make sure to add a todo");
+  res.json(todos, "Make sure to add a todo");
 });
-app.get('/todosws', (req, res) => {
 
-    res.json(todos,"Make sure to add a todo");
-  });
 // Create a new todo
 app.post('/todos', (req, res) => {
   const { title, completed } = req.body;
@@ -56,6 +52,31 @@ app.delete('/todos/:id', (req, res) => {
   } else {
     res.status(404).json({ message: 'Todo not found' });
   }
+});
+
+// Mark all todos as completed
+app.patch('/todos/mark-all-completed', (req, res) => {
+  todos = todos.map((todo) => ({ ...todo, completed: true }));
+  res.json(todos);
+});
+
+// Get a single todo by ID
+app.get('/todos/:id', (req, res) => {
+  const { id } = req.params;
+  const todo = todos.find((t) => t.id === parseInt(id));
+
+  if (todo) {
+    res.json(todo);
+  } else {
+    res.status(404).json({ message: 'Todo not found' });
+  }
+});
+
+// Clear all completed todos
+app.delete('/todos/clear-completed', (req, res) => {
+  const completedTodos = todos.filter((todo) => todo.completed);
+  todos = todos.filter((todo) => !todo.completed);
+  res.json(completedTodos);
 });
 
 // Start the server
